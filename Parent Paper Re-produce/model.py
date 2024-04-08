bfe638764467e0f730c73e08c0be9cb71207a120
Kayla(Mingrui) Chen <61926675+MingruiChen0221@users.noreply.github.com>
2024-04-07 21:16:57 -0400

    Add files via upload

--- Parent Paper Re-produce/model.py
+++ Parent Paper Re-produce/model.py
@@ -8,17 +8,19 @@ class SLCABG(nn.Module):
         super(SLCABG, self).__init__()
         self.word_embeddings = nn.Embedding.from_pretrained(word_vectors)
         self.word_embeddings.weight.requires_grad = False
-        self.convs = nn.ModuleList(
-            [nn.Sequential(
+        self.convs = nn.ModuleList([
+            nn.Sequential(
                 nn.Conv1d(n_dim, 128, h),
                 nn.BatchNorm1d(128),
                 nn.ReLU(),
-                nn.MaxPool1d(sentence_length - h + 1)
-                           ) for h in [3, 4, 5]]
-        )
+                nn.MaxPool1d(sentence_length - h + 1),
+            ) for h in [3, 4, 5]
+        ])
         self.gru = nn.GRU(128*3, 64, batch_first=True, bidirectional=True, dropout=0.4)
+        self.lstm = nn.LSTM(128, 64, batch_first=True, bidirectional=True, dropout=0.5)  # new added LSTM
         self.weight_W = nn.Parameter(torch.Tensor(128, 128))
         self.weight_proj = nn.Parameter(torch.Tensor(128, 1))
+        self.fc_relu = nn.ReLU()  # New Added ReLU
         self.fc = nn.Linear(128, 2)
         nn.init.uniform_(self.weight_W, -0.1, 0.1)
         nn.init.uniform_(self.weight_proj, -0.1, 0.1)
@@ -30,10 +32,12 @@ class SLCABG(nn.Module):
         out = torch.cat(out, 1)
         out = out.permute(0, 2, 1)
         out, _ = self.gru(out)
+        out, _ = self.lstm(out)  # Trough LSTM layer
         u = torch.tanh(torch.matmul(out, self.weight_W))
         att = torch.matmul(u, self.weight_proj)
         att_score = F.softmax(att, dim=1)
         scored_x = out * att_score
         feat = torch.sum(scored_x, dim=1)
+        feat = self.fc_relu(feat)  # Apply ReLu Layer
         out = self.fc(feat)
         return out
